e9e31df7b6631be1a693d7fef8d1d93709e797f0
Kovalenko Yuriy <kovalenko_juri@mail.ru>
2020-10-04 01:26:57 +0300

    Init project

---
+++ tests/simpleTest/SimpleTest.cpp
@@ -0,0 +1,32 @@
+#include "gtest/gtest.h"
+
+namespace nicledomas
+{
+
+namespace test
+{
+
+class Test_SimpleTest : public ::testing::Test
+{
+protected:
+    Test_SimpleTest()
+    {}
+
+    void SetUp() override
+    {
+    }
+
+    void TearDown() override
+    {
+    }
+};
+
+TEST_F(Test_SimpleTest, FirstTest)
+{
+    ASSERT_EQ(1, 1);
+    ASSERT_NE(1, 2);
+}
+
+} // test
+
+} // nicledomas
