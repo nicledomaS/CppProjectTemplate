a801065f25fe0bb8272341e6ab71672a45ea46f3
Yuriy Ko <kovalenko_juri@mail.ru>
2021-04-06 18:16:50 +0300

    Add gsl lib to project

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,12 @@
 #include "pugixml.hpp"
 
+#include <gsl/gsl>
+
 #include <chrono>
 #include <thread>
 #include <iostream>
+
+void print(gsl::not_null<int*> value);
 
 int main (int argc, char** argv)
 {
@@ -10,11 +14,23 @@ int main (int argc, char** argv)
 
     pugi::xml_document doc;
 
+    int* first = new int(5);
+    int* second = nullptr;
+    
+
     while(true)
     {
         std::this_thread::sleep_for(std::chrono::seconds(1));
-        
+        print(first);
+        print(second);
     }
 
+    delete first;
+
     return 0;
+}
+
+void print(gsl::not_null<int*> value)
+{
+    std::cout << *value << std::endl;
 }
